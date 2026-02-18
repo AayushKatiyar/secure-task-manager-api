45fc4c44054d6b33c0d44523f71712ac10737fa5
Aayush Katiyar <krishnakatiyar123@gmail.com>
2026-02-18 11:03:18 +0530

    Initial project setup with gitignore

---
+++ src/test/java/com/aayush/secure_task_manager/SecureTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aayush.secure_task_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
