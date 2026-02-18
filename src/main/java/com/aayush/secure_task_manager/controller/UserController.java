75e7fec3e9d16eb0f12f9907a2f1c4f07cf1c131
Aayush Katiyar <krishnakatiyar123@gmail.com>
2026-02-18 13:00:47 +0530

    Add user registration API endpoint and test database integration

---
+++ src/main/java/com/aayush/secure_task_manager/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.aayush.secure_task_manager.controller;
+
+import com.aayush.secure_task_manager.entity.User;
+import com.aayush.secure_task_manager.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+
+        User savedUser = userService.registerUser(user);
+        return ResponseEntity.ok(savedUser);
+    }
+}
